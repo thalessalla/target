31ce81dca99555db636a68730eb89286339ed615
thalessalla <101899988+thalessalla@users.noreply.github.com>
2023-03-14 19:30:54 -0300

    commit secundário

--- teste 4.py
+++ teste 4.py
@@ -1,4 +1,4 @@
-O carro estará mais próximo de Ribeirão Preto.
+# O carro estará mais próximo de Ribeirão Preto.
 
 
-A conclusão é que, apesar do caminhão ter percorrido uma distância menor em relação ao carro, ele levará mais tempo para chegar em Ribeirão Preto devido aos obstáculos no caminho (pedágios) e à sua velocidade menor. Portanto, quando eles se cruzarem na rodovia, o carro estará mais próximo da cidade de Ribeirão Preto.
+# A conclusão é que, apesar do caminhão ter percorrido uma distância menor em relação ao carro, ele levará mais tempo para chegar em Ribeirão Preto devido aos obstáculos no caminho (pedágios) e à sua velocidade menor. Portanto, quando eles se cruzarem na rodovia, o carro estará mais próximo da cidade de Ribeirão Preto.
